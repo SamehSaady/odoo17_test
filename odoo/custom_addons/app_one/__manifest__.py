6c91ef894d64e3259ab5f6cbb6cbacf0badf2afe
Sameh Saady <samehsaady77@gmail.com>
2024-07-23 17:17:43 +0300

    Initializing first app

---
+++ odoo/custom_addons/app_one/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "App One",
+    'author': "Sameh Saady",
+    'category': "",
+    'version': "17.0.0.1.0",
+    'depends': [
+        "base",
+                ],
+    'data': [
+
+    ],
+    'application': True
+}
